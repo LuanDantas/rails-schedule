0d03d6a36478a6662f62c3fa0bf21e94c7b11d4c
LuanDantas <kidinoop@hotmail.com>
2018-06-20 20:43:24 -0300

    Updated relation between models.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  has_many :phones # has many phones (possui muitos phones)
+  has_one :address # has one address (possui um endereço)
+
+  has_many :contact_kinds # contact has many <intermedium table> (contact possui muitos 'tabela intermediária')
+  has_many :kinds, through: :contact_kinds # contact has many kinds, through of <intermedium table> (contact possui muitos kinds, através da 'tabela intermediária')
 end
--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,2 +1,4 @@
 class Kind < ApplicationRecord
+  has_many :contact_kinds # kind has many <intermedium table> (kind possui muitos 'tabela intermediária')
+  has_many :contacts, through: :contact_kinds # kind has many contacts, through of <intermedium table> (kinds possui muitos contacts, através da 'tabela intermediária')
 end

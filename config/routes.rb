7f6a14fd07001eb2e97b57c59cbbd1d09679ce20
LuanDantas <kidinoop@hotmail.com>
2018-06-20 21:06:19 -0300

    Generate Contacts Controller in project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+  get 'contacts/show'
+  get 'contacts/new'
+  get 'contacts/create'
+  get 'contacts/edit'
+  get 'contacts/update'
+  get 'contacts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

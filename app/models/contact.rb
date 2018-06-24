e22e92a6a7938e858077a5d55a090e42898bcbce
LuanDantas <kidinoop@hotmail.com>
2018-06-24 09:06:18 -0300

    Alter contact's model to accept 'dependent: :destroy' and remove phones and address automatic if contact data was deleted.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
-  has_many :phones # has many phones (possui muitos phones)
-  has_one :address # has one address (possui um endereço)
+  has_many :phones, dependent: :destroy # has many phones (possui muitos phones) - depentend: :destroy will remove the data too if the Contact was deleted.
+  has_one :address, dependent: :destroy # has one address (possui um endereço) - depentend: :destroy will remove the data too if the Contact was deleted.
 
   has_many :contact_kinds # contact has many <intermedium table> (contact possui muitos 'tabela intermediária')
   has_many :kinds, through: :contact_kinds # contact has many kinds, through of <intermedium table> (contact possui muitos kinds, através da 'tabela intermediária')
